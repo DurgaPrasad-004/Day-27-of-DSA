9f58f67892db376172cab98a0057b72338b42b64
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-09 21:39:33 +0530

    Leaders of array Brute force approach.

--- Arrays-X.cpp
+++ Arrays-X.cpp
@@ -167,3 +167,28 @@ int main() {
 }
 
 //Time Complexity: O(2*N)
+
+
+// leaders of array
+vector<int> LeadersOfarray(int arr[], int n){
+    vector<int> ans;
+    for(int i = 0; i < n; i++){
+        bool leader = true;
+
+    //Checking whether arr[i] is greater than all 
+    //the elements in its right side
+        for(int j = i+1; j <n; j++){
+            if(arr[j] > arr[i]){
+
+        // If any element found is greater than current leader
+        // curr element is not the leader
+                leader = false;
+                break;
+            }
+        }
+        if(leader == true)ans.push_back(arr[i]);
+    }
+    return ans;
+}
+
+//Time Complexity: O(N^2);
